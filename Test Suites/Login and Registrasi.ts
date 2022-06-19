4b087565eef4d2879b820cba04373cee527b4a90
ariansyahriwendi <ariansyahriwendi@gmail.com>
2022-06-19 22:38:18 +0700

    Second

---
+++ Test Suites/Login and Registrasi.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login and Registrasi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c0b64d18-ade9-40ae-908c-615bce64e1c0</testSuiteGuid>
+</TestSuiteEntity>
